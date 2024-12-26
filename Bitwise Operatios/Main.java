c806086f64ca8bb1fdd9bc681d030133bdd0733e
mdmonis <mdmonis128@gmail.com>
2024-12-26 13:51:44 +0530

    Binary to Decimal conversion

--- Bitwise Operatios/Main.java
+++ Bitwise Operatios/Main.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.println("Enter the number: ");
        int n = sc.nextInt();
        String s = "";
-       while(n<0){
-        s = s;
-         System.out.println(s);
+       while(n>0){
+        s = n%2 + s;
         n = n/2;
-         System.out.println(n);
        }
-    System.out.println(s);
+    System.out.print(s);
     }
 }
